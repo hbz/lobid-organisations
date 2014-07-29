a4e28c3aacc9c8f5d20d50ba0cd5c8db8f7daaae
Fabian Steeg <fsteeg@gmail.com>
2014-07-29 12:20:18 +0200

    Enrich DBS records with Sigel data based on the DBS ID
    - exclude NULL values in DBS records
    - exclude Sigel records without DBS reference
    - restore DBS ID (`inr`) for JSON output

--- src/main/java/EnrichDbsWithSigelToJson.java
+++ src/main/java/EnrichDbsWithSigelToJson.java
@@ -1,6 +1,8 @@
+import org.culturegraph.mf.morph.Metamorph;
 import org.culturegraph.mf.stream.converter.JsonEncoder;
 import org.culturegraph.mf.stream.converter.StreamToTriples;
 import org.culturegraph.mf.stream.pipe.CloseSupressor;
+import org.culturegraph.mf.stream.pipe.TripleFilter;
 import org.culturegraph.mf.stream.pipe.sort.AbstractTripleSort.Compare;
 import org.culturegraph.mf.stream.pipe.sort.TripleCollect;
 import org.culturegraph.mf.stream.pipe.sort.TripleSort;
@@ -10,52 +12,51 @@ import org.culturegraph.mf.stream.source.FileOpener;
 import org.culturegraph.mf.types.Triple;
 
 /**
- * Initial simple merging of DBS and Sigel records based on their ISIL.
+ * Simple enrichment of DBS records with Sigel data based on the DBS ID.
  * 
- * After merging, the result is transformed to JSON.
+ * After enrichment, the result is transformed to JSON.
  * 
  * @author Fabian Steeg (fsteeg)
  *
  */
-public class SigelAndDbsToJson {
+public class EnrichDbsWithSigelToJson {
 
 	/** @param args Not used */
 	public static void main(String[] args) {
-
 		DirReader openSigel = new DirReader();
-
 		StreamToTriples streamToTriples1 = new StreamToTriples();
 		streamToTriples1.setRedirect(true);
 		StreamToTriples flow1 = //
 				SigelToJson.morphSigel(openSigel).setReceiver(streamToTriples1);
-
 		FileOpener openDbs = new FileOpener();
 		StreamToTriples streamToTriples2 = new StreamToTriples();
 		streamToTriples2.setRedirect(true);
 		StreamToTriples flow2 = //
 				DbsToJson.morphDbs(openDbs).setReceiver(streamToTriples2);
-
 		CloseSupressor<Triple> wait = new CloseSupressor<>(2);
 		continueWith(flow1, wait);
 		continueWith(flow2, wait);
-
 		DbsToJson.processDbs(openDbs);
 		SigelToJson.processSigel(openSigel);
 	}
 
 	private static void continueWith(StreamToTriples flow,
 			CloseSupressor<Triple> wait) {
-
+		TripleFilter tripleFilter = new TripleFilter();
+		tripleFilter.setSubjectPattern(".+"); // remove Sigel entries w/o DBS link
+		Metamorph restoreDbsId =
+				new Metamorph("src/main/resources/morph/restore-dbs-id.morph.xml");
 		TripleSort sortTriples = new TripleSort();
 		sortTriples.setBy(Compare.SUBJECT);
 		JsonEncoder encodeJson = new JsonEncoder();
 		encodeJson.setPrettyPrinting(true);
 		ObjectWriter<String> writer =
-				new ObjectWriter<>("src/main/resources/output/sigel-dbs.out.json");
-
+				new ObjectWriter<>("src/main/resources/output/dbs-enriched.out.json");
 		flow.setReceiver(wait)//
+				.setReceiver(tripleFilter)//
 				.setReceiver(sortTriples)//
 				.setReceiver(new TripleCollect())//
+				.setReceiver(restoreDbsId)//
 				.setReceiver(encodeJson)//
 				.setReceiver(writer);
 	}
