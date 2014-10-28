22ffb7b4a1d7ae77ecf05d1517db54c56e891911
Simon Ritter <simon.b.ritter@gmail.com>
2014-10-28 14:12:01 +0100

    Separate indexing from transformation and edit README
    - Indexing is now run separately from transformation (class Index)
    - All recent changes are recorded in README file

--- src/main/java/flow/Enrich.java
+++ src/main/java/flow/Enrich.java
@@ -1,14 +1,5 @@
 package flow;
 
-import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
-
 import org.culturegraph.mf.morph.Metamorph;
 import org.culturegraph.mf.stream.converter.JsonEncoder;
 import org.culturegraph.mf.stream.converter.JsonToElasticsearchBulk;
@@ -22,21 +13,6 @@ import org.culturegraph.mf.stream.sink.ObjectWriter;
 import org.culturegraph.mf.stream.source.DirReader;
 import org.culturegraph.mf.stream.source.FileOpener;
 import org.culturegraph.mf.types.Triple;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.node.Node;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Simple enrichment of DBS records with Sigel data based on the DBS ID.
@@ -48,25 +24,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class Enrich {
 
-	private static Node node;
-	private static Client client;
-
 	/**
 	 * @param args Not used
-	 * @throws IOException if json file with output cannot be found
-	 * @throws JsonMappingException if json mapping of organisation data cannot be
-	 *           created
-	 * @throws JsonParseException if value cannot be read from json mapper
 	 */
-	public static void main(String... args) throws JsonParseException,
-			JsonMappingException, IOException {
+	public static void main(String... args) {
 		/* Run both preparatory pipelines standalone for debugging, doc etc. */
 		Dbs.main();
 		Sigel.main();
 		/* Run the actual enrichment pipeline, which includes the previous: */
 		process();
-		/* Build index from output on elasticsearch */
-		indexToElasticSearch();
 	}
 
 	static void process() {
@@ -108,81 +74,5 @@ public class Enrich {
 				.setReceiver(encodeJson)//
 				.setReceiver(esBulk)//
 				.setReceiver(writer);
-	}
-
-	private static void indexToElasticSearch() throws JsonParseException,
-			JsonMappingException, IOException {
-		node = nodeBuilder().local(false).node();
-		Settings clientSettings =
-				ImmutableSettings.settingsBuilder()
-						.put("cluster.name", "organisation-cluster")
-						.put("client.transport.sniff", true).build();
-
-		// Funktioniert das nicht?
-		try (TransportClient transportClient = new TransportClient(clientSettings)) {
-			client =
-					transportClient.addTransportAddress(new InetSocketTransportAddress(
-							"weywot2.hbz-nrw.de", 9300));
-
-			createEmptyIndex();
-			indexData();
-			client.close();
-			node.close();
-		}
-	}
-
-	private static void createEmptyIndex() throws IOException {
-		deleteIndex();
-		String settingsMappings =
-				Files.lines(Paths.get("src/main/resources/index-settings.json"))
-						.collect(Collectors.joining());
-		CreateIndexRequestBuilder cirb =
-				client.admin().indices().prepareCreate("organisations");
-		cirb.setSource(settingsMappings);
-		cirb.execute().actionGet();
-	}
-
-	private static void indexData() throws IOException {
-		BulkRequestBuilder bulkRequest = client.prepareBulk();
-		try (BufferedReader br =
-				new BufferedReader(new FileReader(
-						"src/main/resources/output/enriched.out.json"))) {
-			readData(bulkRequest, br);
-		}
-		bulkRequest.execute().actionGet();
-		client.admin().indices().refresh(new RefreshRequest()).actionGet();
-	}
-
-	private static void readData(BulkRequestBuilder bulkRequest, BufferedReader br)
-			throws IOException, JsonParseException, JsonMappingException {
-		ObjectMapper mapper = new ObjectMapper();
-		String line;
-		int currentLine = 1;
-		String organisationId = null;
-		String organisationData = null;
-
-		// First line: index with id, second line: source
-		while ((line = br.readLine()) != null) {
-			if (currentLine % 2 != 0) {
-				JsonNode rootNode = mapper.readValue(line, JsonNode.class);
-				JsonNode index = rootNode.get("index");
-				organisationId = index.findValue("_id").asText();
-
-			} else {
-				organisationData = line;
-				bulkRequest.add(client.prepareIndex("organisations", "dbs",
-						organisationId).setSource(organisationData));
-			}
-			currentLine++;
-		}
-	}
-
-	private static void deleteIndex() {
-		if (client.admin().indices().prepareExists("organisations").execute()
-				.actionGet().isExists()) {
-			DeleteIndexRequest deleteIndexRequest =
-					new DeleteIndexRequest("organisations");
-			client.admin().indices().delete(deleteIndexRequest);
-		}
 	}
 }
