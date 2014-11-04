f592d93cc72ce966304cfb6c11904883a50ba7d1
Simon Ritter <simon.b.ritter@gmail.com>
2014-11-04 10:28:32 +0100

    Handle possible errors in geo search
    - Check whether polygon has more than two points
    - Check whether distance is greater than 0
    - Revise code for better readability

--- web/app/controllers/Application.java
+++ web/app/controllers/Application.java
@@ -1,6 +1,5 @@
 package controllers;
 
-
 import java.io.IOException;
 
 import org.elasticsearch.action.search.SearchResponse;
@@ -18,29 +17,30 @@ import org.elasticsearch.index.query.GeoPolygonFilterBuilder;
 import org.elasticsearch.index.query.MatchAllFilterBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 
+import play.Play;
+import play.libs.F.Promise;
+import play.libs.ws.WS;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import play.*;
-import play.libs.F.Promise;
-import play.libs.ws.WS;
-import play.mvc.*;
-import views.html.*;
 
 public class Application extends Controller {
 
 	private static final String ES_SERVER = "http://weywot2.hbz-nrw.de:9200";
 	private static final String ES_INDEX = "organisations";
 	private static final String ES_TYPE = "dbs";
-	
-	private static Settings clientSettings =
-			ImmutableSettings.settingsBuilder()
-					.put("cluster.name", "organisation-cluster")
-					.put("client.transport.sniff", true).build();
-	private static TransportClient transportClient = new TransportClient(clientSettings);
-	private static Client client =
-			transportClient.addTransportAddress(new InetSocketTransportAddress(
+
+	private static Settings clientSettings = ImmutableSettings
+			.settingsBuilder().put("cluster.name", "organisation-cluster")
+			.put("client.transport.sniff", true).build();
+	private static TransportClient transportClient = new TransportClient(
+			clientSettings);
+	private static Client client = transportClient
+			.addTransportAddress(new InetSocketTransportAddress(
 					"weywot2.hbz-nrw.de", 9300));
 
 	public static Result index() {
@@ -52,81 +52,126 @@ public class Application extends Controller {
 		response().setHeader("Access-Control-Allow-Origin", "*");
 		return ok(Play.application().resourceAsStream("context.jsonld"));
 	}
-	
-	public static Result search(String q, String location, int from, int size) throws JsonProcessingException, IOException {
+
+	public static Result search(String q, String location, int from, int size)
+			throws JsonProcessingException, IOException {
 		String[] queryParts = q.split(":");
 		String field = queryParts[0];
-		String term = queryParts[1];		
-		SearchResponse queryResponse;
-		
-		if(location == null) {
-			queryResponse = querySimple(field, term, from, size);			
-		} else {			
-			String[] coordPairsAsString = location.split(" ");
-			
-			/* if there is a list of three elements separated by a comma:
-			interpretation as point (first + second value) and distance (third value) */
-			if(coordPairsAsString[0].split(",").length > 2){
-				double lat = Double.parseDouble(coordPairsAsString[0].split(",")[0]);
-				double lon = Double.parseDouble(coordPairsAsString[0].split(",")[1]);
-				double distance = Double.parseDouble(coordPairsAsString[0].split(",")[2]); 
-				queryResponse = queryDistance(field, term, from, size, lat, lon, distance);				
-			} 
-			
-			/* if not, interpretation as list of polygon points */
-			else {
-				double[] latCoordinates = new double[coordPairsAsString.length];
-				double[] lonCoordinates = new double[coordPairsAsString.length];
-				for(int i = 0; i < coordPairsAsString.length; i++){
-					latCoordinates[i] = Double.parseDouble(coordPairsAsString[i].split(",")[0]);
-					lonCoordinates[i] = Double.parseDouble(coordPairsAsString[i].split(",")[1]);
-				}
-				queryResponse = queryPolygon(field, term, latCoordinates, lonCoordinates, from, size);
-			}
+		String term = queryParts[1];
+		Status result = null;
+		if (location == null) {
+			result = buildSimpleQuery(field, term, from, size);
+		} else {
+			result = prepareLocationQuery(location, field, term, from, size);
 		}
-		JsonNode responseAsJson = new ObjectMapper().readTree(queryResponse.toString());
-		return ok(responseAsJson);
-	}
-	
-	private static SearchResponse querySimple(String field, String term, int from, int size) {		
-		MatchAllFilterBuilder matchAllFilter = FilterBuilders.matchAllFilter();
-		FilteredQueryBuilder simpleQuery =
-				QueryBuilders.filteredQuery(QueryBuilders.matchQuery(field, term), matchAllFilter);
-		return executeQuery(from, size, simpleQuery);
-	}
-	
-	private static SearchResponse queryPolygon(String field, String term, double[] latCoordinates, double[] lonCoordinates, int from, int size) {
-		GeoPolygonFilterBuilder polygonFilter =
-				FilterBuilders.geoPolygonFilter("location");
-		
-		for(int i = 0; i < latCoordinates.length; i++){
-			polygonFilter.addPoint(latCoordinates[i], lonCoordinates[i]);
-		}
-		
-		FilteredQueryBuilder polygonQuery =
-				QueryBuilders.filteredQuery(QueryBuilders.matchQuery(field, term), polygonFilter);
-		return executeQuery(from, size, polygonQuery);
-	}
-	
-	private static SearchResponse queryDistance(String field, String term, int from, int size, double lat, double lon, double distance){
-		GeoDistanceFilterBuilder distanceFilter =
-				FilterBuilders.geoDistanceFilter("location").distance(distance, DistanceUnit.KILOMETERS).point(lat, lon);
-		FilteredQueryBuilder distanceQuery =
-				QueryBuilders.filteredQuery(QueryBuilders.matchQuery(field, term), distanceFilter);
-		return executeQuery(from, size, distanceQuery);
+		return result;
 	}
 
-	private static SearchResponse executeQuery(int from, int size, FilteredQueryBuilder filteredQuery) {
-		SearchResponse responseOfSearch =
-				client.prepareSearch("organisations").setTypes("dbs")
-						.setSearchType(SearchType.QUERY_THEN_FETCH).setQuery(filteredQuery)
-						.setFrom(from).setSize(size)
-						.execute().actionGet();
+	private static Status prepareLocationQuery(String location, String field,
+			String term, int from, int size) throws JsonProcessingException,
+			IOException {
+		String[] coordPairsAsString = location.split(" ");
+		Status result;
+		if (coordPairsAsString[0].split(",").length > 2) {
+			result = prepareDistanceQuery(coordPairsAsString, field, term,
+					from, size);
+		} else {
+			result = preparePolygonQuery(coordPairsAsString, field, term, from,
+					size);
+		}
+		return result;
+	}
+
+	private static Status preparePolygonQuery(String[] coordPairsAsString,
+			String field, String term, int from, int size)
+			throws JsonProcessingException, IOException {
+		double[] latCoordinates = new double[coordPairsAsString.length];
+		double[] lonCoordinates = new double[coordPairsAsString.length];
+		Status result;
+		for (int i = 0; i < coordPairsAsString.length; i++) {
+			String[] coordinatePair = coordPairsAsString[i].split(",");
+			latCoordinates[i] = Double.parseDouble(coordinatePair[0]);
+			lonCoordinates[i] = Double.parseDouble(coordinatePair[1]);
+		}
+		if (coordPairsAsString.length < 3) {
+			return badRequest("Not enough points. Polygon requires more than two points.");
+		} else {
+			result = buildPolygonQuery(field, term, latCoordinates, lonCoordinates,
+					from, size);
+		}
+		return result;
+	}
+
+	private static Status prepareDistanceQuery(String[] coordPairsAsString,
+			String field, String term, int from, int size)
+			throws JsonProcessingException, IOException {
+		String[] coordinatePair = coordPairsAsString[0].split(",");
+		double lat = Double.parseDouble(coordinatePair[0]);
+		double lon = Double.parseDouble(coordinatePair[1]);
+		double distance = Double.parseDouble(coordinatePair[2]);
+		Status result;
+		if (distance < 0) {
+			return badRequest("Distance must not be smaller than 0.");
+		} else {
+			result = buildDistanceQuery(field, term, from, size, lat, lon, distance);
+		}
+		return result;
+	}
+
+	private static Status buildSimpleQuery(String field, String term, int from,
+			int size) throws JsonProcessingException, IOException {
+		MatchAllFilterBuilder matchAllFilter = FilterBuilders.matchAllFilter();
+		FilteredQueryBuilder simpleQuery = QueryBuilders.filteredQuery(
+				QueryBuilders.matchQuery(field, term), matchAllFilter);
+		SearchResponse queryResponse = executeQuery(from, size, simpleQuery);
+		return returnAsJson(queryResponse);
+	}
+
+	private static Status buildPolygonQuery(String field, String term,
+			double[] latCoordinates, double[] lonCoordinates, int from, int size)
+			throws JsonProcessingException, IOException {
+		GeoPolygonFilterBuilder polygonFilter = FilterBuilders
+				.geoPolygonFilter("location");
+		for (int i = 0; i < latCoordinates.length; i++) {
+			polygonFilter.addPoint(latCoordinates[i], lonCoordinates[i]);
+		}
+		FilteredQueryBuilder polygonQuery = QueryBuilders.filteredQuery(
+				QueryBuilders.matchQuery(field, term), polygonFilter);
+		SearchResponse queryResponse = executeQuery(from, size, polygonQuery);
+		return returnAsJson(queryResponse);
+	}
+
+	private static Status buildDistanceQuery(String field, String term, int from,
+			int size, double lat, double lon, double distance)
+			throws JsonProcessingException, IOException {
+		GeoDistanceFilterBuilder distanceFilter = FilterBuilders
+				.geoDistanceFilter("location")
+				.distance(distance, DistanceUnit.KILOMETERS).point(lat, lon);
+		FilteredQueryBuilder distanceQuery = QueryBuilders.filteredQuery(
+				QueryBuilders.matchQuery(field, term), distanceFilter);
+		SearchResponse queryResponse = executeQuery(from, size, distanceQuery);
+		return returnAsJson(queryResponse);
+	}
+
+	private static SearchResponse executeQuery(int from, int size,
+			FilteredQueryBuilder filteredQuery) {
+		SearchResponse responseOfSearch = client.prepareSearch("organisations")
+				.setTypes("dbs").setSearchType(SearchType.QUERY_THEN_FETCH)
+				.setQuery(filteredQuery).setFrom(from).setSize(size).execute()
+				.actionGet();
 		return responseOfSearch;
 	}
 
+	private static Status returnAsJson(SearchResponse queryResponse)
+			throws IOException, JsonProcessingException {
+		JsonNode responseAsJson = new ObjectMapper().readTree(queryResponse
+				.toString());
+		return ok(responseAsJson);
+	}
+
 	public static Promise<Result> get(String id) {
-		String url = String.format("%s/%s/%s/%s/_source", ES_SERVER, ES_INDEX, ES_TYPE, id);
+		String url = String.format("%s/%s/%s/%s/_source", ES_SERVER, ES_INDEX,
+				ES_TYPE, id);
 		return WS.url(url).execute().map(x -> ok(x.asJson()));
 	}
 }
