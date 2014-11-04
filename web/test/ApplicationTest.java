bd9d0d487775ba37bc5f8a7ed6ffe4dea4ad5aa9
Simon Ritter <simon.b.ritter@gmail.com>
2014-11-04 15:48:15 +0100

    Replace settings of the project and revise the code accordingly
    - Replace org.eclipse.jdt.core.prefs and org.eclipse.jdt.ui.prefs
    - Revise code according to formatting rules and warnings
    - Add javadoc comments

--- web/test/ApplicationTest.java
+++ web/test/ApplicationTest.java
@@ -1,30 +1,38 @@
-import org.junit.*;
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.header;
+import static play.test.Helpers.route;
+import static play.test.Helpers.running;
 
-import play.mvc.*;
+import org.junit.Test;
+
+import play.mvc.Result;
 import play.twirl.api.Content;
-import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
-
 
 /**
  * Tests without external dependencies. Run in CI.
  */
+@SuppressWarnings("javadoc")
 public class ApplicationTest {
 
-    @Test
-    public void renderIndexTemplate() {
-        Content html = views.html.index.render("Page title");
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Page title");
-    }
+	@Test
+	public void renderIndexTemplate() {
+		Content html = views.html.index.render("Page title");
+		assertThat(contentType(html)).isEqualTo("text/html");
+		assertThat(contentAsString(html)).contains("Page title");
+	}
 
-    @Test
-    public void contextContentTypeAndCorsHeader() {
-        running(fakeApplication(), () -> {
-             Result result = route(fakeRequest(GET, "/organisations/context.jsonld"));
-             assertThat(result).isNotNull();
-             assertThat(contentType(result)).isEqualTo("application/ld+json");
-             assertThat(header("Access-Control-Allow-Origin", result)).isEqualTo("*");
-        });
-    }
+	@Test
+	public void contextContentTypeAndCorsHeader() {
+		running(fakeApplication(), () -> {
+			Result result = route(fakeRequest(GET, "/organisations/context.jsonld"));
+			assertThat(result).isNotNull();
+			assertThat(contentType(result)).isEqualTo("application/ld+json");
+			assertThat(header("Access-Control-Allow-Origin", result)).isEqualTo("*");
+		});
+	}
 }
--- web/test/IntegrationTest.java
+++ web/test/IntegrationTest.java
@@ -1,82 +1,117 @@
-import org.junit.*;
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.HTMLUNIT;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.inMemoryDatabase;
+import static play.test.Helpers.route;
+import static play.test.Helpers.running;
+import static play.test.Helpers.testServer;
 
-import play.mvc.*;
-import play.test.*;
-import play.libs.F.*;
-import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
+import org.junit.Test;
+
+import play.libs.F.Callback;
+import play.mvc.Result;
+import play.test.TestBrowser;
 
 /**
  * Tests with internal dependencies. Run locally.
  */
+@SuppressWarnings("javadoc")
 public class IntegrationTest {
 
-    @Test
-    public void testMainPage() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333/organisations");
-                assertThat(browser.pageSource()).contains("lobid-organisations");
-            }
-        });
-    }
+	@Test
+	public void testMainPage() {
+		running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT,
+				new Callback<TestBrowser>() {
+					@Override
+					public void invoke(TestBrowser browser) {
+						browser.goTo("http://localhost:3333/organisations");
+						assertThat(browser.pageSource()).contains("lobid-organisations");
+					}
+				});
+	}
 
-    @Test
-    public void getById() {
-        running(fakeApplication(), () -> {
-             Result result = route(fakeRequest(GET, "/organisations/ZY571"));
-             assertThat(result).isNotNull();
-             assertThat(contentType(result)).isEqualTo("application/json");
-             assertThat(contentAsString(result)).contains("HBZ");
-        });
-    }
+	@Test
+	public void getById() {
+		running(fakeApplication(), () -> {
+			Result result = route(fakeRequest(GET, "/organisations/ZY571"));
+			assertThat(result).isNotNull();
+			assertThat(contentType(result)).isEqualTo("application/json");
+			assertThat(contentAsString(result)).contains("HBZ");
+		});
+	}
 
-    @Test
-    public void queryByField() {
-        running(fakeApplication(), () -> {
-             Result result = route(fakeRequest(GET, "/organisations/search?q=fundertype.value:land&size=2000"));
-             assertThat(result).isNotNull();
-             assertThat(contentType(result)).isEqualTo("application/json");
-             assertThat(contentAsString(result)).contains("Universitätsbibliothek Dortmund");
-        });
-    }
-    
-    @Test
-    public void rectangleSearch(){
-    	running(fakeApplication(), () -> {
-    		Result result = route(fakeRequest(GET, "/organisations/search?q=fundertype.value:land&location=52,12+53,12+53,14+52,14"));
-    		assertThat(result).isNotNull();
-    		assertThat(contentType(result)).isEqualTo("application/json");
-    		assertThat(contentAsString(result)).contains("Berlin");
-    	});
-    }
-    
-    @Test
-    public void triangleSearch(){
-    	running(fakeApplication(), () -> {
-    		Result result = route(fakeRequest(GET, "/organisations/search?q=fundertype.value:land&location=54,15+56,14+56,12"));
-    		assertThat(result).isNotNull();
-    		assertThat(contentType(result)).isEqualTo("application/json");
-    	});
-    }
-    
-    @Test
-    public void hexagonSearch(){
-    	running(fakeApplication(), () -> {
-    		Result result = route(fakeRequest(GET, "/organisations/search?q=fundertype.value:land&location=54,15+56,14+56,12+54,10+52,11+53,14"));
-    		assertThat(result).isNotNull();
-    		assertThat(contentType(result)).isEqualTo("application/json");
-    	});
-    }
-    
-    @Test
-    public void distanceSearch(){
-    	running(fakeApplication(), () -> {
-    		Result result = route(fakeRequest(GET, "/organisations/search?q=fundertype.value:land&location=52.52,13.39,25"));
-    		assertThat(result).isNotNull();
-    		assertThat(contentType(result)).isEqualTo("application/json");
-    		assertThat(contentAsString(result)).contains("Berlin");
-    	});
-    }
-    
+	@Test
+	public void queryByField() {
+		running(
+				fakeApplication(),
+				() -> {
+					Result result =
+							route(fakeRequest(GET,
+									"/organisations/search?q=fundertype.value:land&size=2000"));
+					assertThat(result).isNotNull();
+					assertThat(contentType(result)).isEqualTo("application/json");
+					assertThat(contentAsString(result)).contains(
+							"Universitätsbibliothek Dortmund");
+				});
+	}
+
+	@Test
+	public void rectangleSearch() {
+		running(
+				fakeApplication(),
+				() -> {
+					Result result =
+							route(fakeRequest(
+									GET,
+									"/organisations/search?q=fundertype.value:land&location=52,12+53,12+53,14+52,14"));
+					assertThat(result).isNotNull();
+					assertThat(contentType(result)).isEqualTo("application/json");
+					assertThat(contentAsString(result)).contains("Berlin");
+				});
+	}
+
+	@Test
+	public void triangleSearch() {
+		running(
+				fakeApplication(),
+				() -> {
+					Result result =
+							route(fakeRequest(GET,
+									"/organisations/search?q=fundertype.value:land&location=54,15+56,14+56,12"));
+					assertThat(result).isNotNull();
+					assertThat(contentType(result)).isEqualTo("application/json");
+				});
+	}
+
+	@Test
+	public void hexagonSearch() {
+		running(
+				fakeApplication(),
+				() -> {
+					Result result =
+							route(fakeRequest(
+									GET,
+									"/organisations/search?q=fundertype.value:land&location=54,15+56,14+56,12+54,10+52,11+53,14"));
+					assertThat(result).isNotNull();
+					assertThat(contentType(result)).isEqualTo("application/json");
+				});
+	}
+
+	@Test
+	public void distanceSearch() {
+		running(
+				fakeApplication(),
+				() -> {
+					Result result =
+							route(fakeRequest(GET,
+									"/organisations/search?q=fundertype.value:land&location=52.52,13.39,25"));
+					assertThat(result).isNotNull();
+					assertThat(contentType(result)).isEqualTo("application/json");
+					assertThat(contentAsString(result)).contains("Berlin");
+				});
+	}
 }
