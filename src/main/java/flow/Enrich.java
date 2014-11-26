204c4597e023817464fe24d0d37385aee71b844b
Simon Ritter <simon.b.ritter@gmail.com>
2014-11-26 11:51:32 +0100

    Configure update process of Sigel data
    Pass arguments to main method of Enrich.java to specify the start
    point and the interval size in days; these values are used while
    loading the Sigel updates from the dnb repository

--- src/main/java/flow/Enrich.java
+++ src/main/java/flow/Enrich.java
@@ -27,7 +27,7 @@ import org.culturegraph.mf.types.Triple;
  * 
  * After enrichment, the result is transformed to JSON for ES indexing.
  * 
- * @author Fabian Steeg (fsteeg)
+ * @author Fabian Steeg (fsteeg), Simon Ritter (SBRitter)
  *
  */
 public class Enrich {
@@ -37,18 +37,19 @@ public class Enrich {
 	private static String sigelDnbRepo = "http://services.d-nb.de/oai/repository";
 
 	/**
-	 * @param args Not used
+	 * @param args start date of Sigel updates (date of Sigel base dump) and size
+	 *          of update intervals in days
 	 */
 	public static void main(String... args) {
-		/* Run both preparatory pipelines standalone for debugging, doc etc. */
-		// Dbs.main();
-		// Sigel.main();
-		/* Run the actual enrichment pipeline, which includes the previous: */
-		process();
+		String startOfUpdates = args[0];
+		int intervalSize = Integer.parseInt(args[1]);
+		process(startOfUpdates, intervalSize);
 	}
 
-	static void process() {
-		int updateIntervals = calculateIntervals("2013-06-01", Sigel.getToday());
+	static void process(String startOfUpdates, int intervalSize) {
+		String start = startOfUpdates;
+		int updateIntervals =
+				calculateIntervals(start, Sigel.getToday(), intervalSize);
 		CloseSupressor<Triple> wait = new CloseSupressor<>(updateIntervals + 2);
 
 		FileOpener openSigelDump = new FileOpener();
@@ -58,11 +59,9 @@ public class Enrich {
 				Sigel.morphSigel(openSigelDump).setReceiver(streamToTriplesDump);
 		continueWith(flowSigelDump, wait);
 
-		String start = "2013-06-01";
-		String end = addDays(start);
+		String end = addDays(start, intervalSize);
 		ArrayList<OaiPmhOpener> updateOpenerList = new ArrayList<>();
 		for (int i = 0; i < updateIntervals; i++) {
-			System.out.println("***" + start + "; " + end);
 			OaiPmhOpener openSigelUpdates = Sigel.createOaiPmhOpener(start, end);
 			StreamToTriples streamToTriplesUpdates = new StreamToTriples();
 			streamToTriplesUpdates.setRedirect(true);
@@ -71,11 +70,11 @@ public class Enrich {
 							.setReceiver(streamToTriplesUpdates);
 			continueWith(flowUpdates, wait);
 			updateOpenerList.add(openSigelUpdates);
-			start = addDays(start);
+			start = addDays(start, intervalSize);
 			if (i == updateIntervals - 2)
 				end = Sigel.getToday();
 			else
-				end = addDays(end);
+				end = addDays(end, intervalSize);
 		}
 
 		FileOpener openDbs = new FileOpener();
@@ -91,14 +90,14 @@ public class Enrich {
 		Dbs.processDbs(openDbs);
 	}
 
-	private static String addDays(String start) {
+	private static String addDays(String start, int intervalSize) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		String result = null;
 		try {
 			Date startDate = dateFormat.parse(start);
 			Calendar calender = Calendar.getInstance();
 			calender.setTime(startDate);
-			calender.add(Calendar.DATE, 50);
+			calender.add(Calendar.DATE, intervalSize);
 			result = dateFormat.format(calender.getTime());
 		} catch (ParseException e) {
 			e.printStackTrace();
@@ -106,11 +105,12 @@ public class Enrich {
 		return result;
 	}
 
-	private static int calculateIntervals(String start, String end) {
+	private static int calculateIntervals(String start, String end,
+			int intervalSize) {
 		LocalDate startDate = LocalDate.parse(start);
 		LocalDate endDate = LocalDate.parse(end);
 		long timeSpan = startDate.until(endDate, ChronoUnit.DAYS);
-		return (int) timeSpan / 50;
+		return (int) timeSpan / intervalSize;
 	}
 
 	private static void continueWith(StreamToTriples flow,
@@ -136,7 +136,7 @@ public class Enrich {
 				.setReceiver(writer);
 	}
 
-	/* For tests */
+	/* For tests: sample data only, no updates */
 	static void processSample() {
 		FileOpener openSigelDump = new FileOpener();
 		StreamToTriples streamToTriples1 = new StreamToTriples();
