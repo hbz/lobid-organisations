763239e9e1c74bb1b1b2f59861754277964e574b
Fabian Steeg <fsteeg@gmail.com>
2016-04-20 14:56:20 +0200

    Add cron.sh script called by cron job for daily updates (see #130)

---
+++ cron.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail # See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+IFS=$'\n\t'
+
+# Execute via crontab by sol@quaoar1:
+# 00 6 * * * cd /home/sol/git/lobid-organisations ; bash cron.sh > logs/cron.sh.log 2>&1
+
+LOGFILE="logs/$(date "+%Y%m%d").log"
+mvn clean install -DskipTests > $LOGFILE 2>&1
+mvn exec:java -Dexec.mainClass=flow.Enrich -Dexec.args="2013-06-01 100 'http://gaia.hbz-nrw.de:7400'" > $LOGFILE 2>&1
+mvn exec:java -Dexec.mainClass=flow.Index -Dexec.args="'23000000' 'src/main/resources/output/enriched.out.json'" > $LOGFILE 2>&1
+
