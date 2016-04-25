a2817c6451ec97a612b8a52394d18964f166c8f0
Simon Ritter <simon.b.ritter@gmail.com>
2016-04-25 13:52:03 +0200

    Call indexing during start up of Play app (#136)

---
+++ web/app/global/Global.java
@@ -0,0 +1,21 @@
+package global;
+
+import java.io.IOException;
+
+import controllers.Index;
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+
+public class Global extends GlobalSettings {
+
+	@Override
+	public void onStart(Application app) {
+		Logger.info("Application has started. Starting indexing now.");
+		try {
+			Index.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
