f74001b9faf7c8472156931d51635a35833ab99f
Simon Ritter <simon.b.ritter@gmail.com>
2016-06-28 19:06:38 +0200

    Add test for exclusion of type Collection (#207)

---
+++ test/index/TestCollection.java
@@ -0,0 +1,16 @@
+package index;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+@SuppressWarnings("javadoc")
+public class TestCollection extends ElasticsearchTest {
+
+	@Test
+	public void searchForCollection() {
+		long total = search("type", "Collection").getHits().getTotalHits();
+		assertEquals("Request should return 0", 0, total);
+	}
+
+}
