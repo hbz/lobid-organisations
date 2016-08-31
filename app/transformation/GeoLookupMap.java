25bb79b370ca86e769f544bbb4be5e750d2c1934
Fabian Steeg <fsteeg@gmail.com>
2016-08-31 17:01:53 +0200

    Look up geodata using Mapzen search API (#89)
    See https://mapzen.com/documentation/search/
    
    Uses Pelias geocoder, see https://github.com/pelias/pelias

--- app/transformation/GeoLookupMap.java
+++ app/transformation/GeoLookupMap.java
@@ -1,14 +1,17 @@
 package transformation;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import controllers.Application;
 import play.Logger;
+import play.cache.Cache;
 import play.libs.F.Promise;
 import play.libs.ws.WS;
+import play.libs.ws.WSRequestHolder;
 import play.mvc.Http.Status;
 
 /**
@@ -20,8 +23,10 @@ import play.mvc.Http.Status;
 public class GeoLookupMap extends HashMap<String, String> {
 
 	private static final long serialVersionUID = 1L;
-	private static final String SERVER =
+	private static final String API =
 			Application.CONFIG.getString("transformation.geo.lookup.server");
+	private static final String API_KEY =
+			Application.CONFIG.getString("transformation.geo.lookup.key");
 	private LookupType lookupType;
 
 	static enum LookupType {
@@ -37,33 +42,54 @@ public class GeoLookupMap extends HashMap<String, String> {
 
 	@Override
 	public String get(Object key) {
-		// TODO avoid duplicate lat/long calls for same key
-		// (will be possible after https://github.com/hbz/geodata/issues/27)
-		// TODO pass key as query param, not in URI path
-		// (will be possible after https://github.com/hbz/geodata/issues/26)
+		JsonNode cached = (JsonNode) Cache.get(key.toString());
+		if (cached != null) {
+			return resultFromNode(cached);
+		}
 		if (!key.equals("__default")) {
-			try {
-				String path =
-						"/geodata" + (lookupType == LookupType.LON ? "/long/" : "/lat/")
-								+ key.toString();
-				URI uri = new URI("http", null, SERVER.split(":")[0],
-						Integer.parseInt(SERVER.split(":")[1]), path, null, null);
-				String url = uri.toASCIIString();
-				Promise<String> promise = WS.url(url).get().map(response -> {
-					if (response.getStatus() == Status.OK) {
-						return response.getBody();
+			String[] strings = key.toString().split("/");
+			WSRequestHolder requestHolder = WS.url(API)//
+					.setQueryParameter("api_key", API_KEY)
+					.setQueryParameter("layers", "address")
+					.setQueryParameter("boundary.country", strings[2].trim())
+					.setQueryParameter("text", strings[0] + " " + strings[1]);
+			Logger.debug("Calling API={} with params={}", requestHolder.getUrl(),
+					requestHolder.getQueryParameters());
+			Promise<String> promise = requestHolder.get().map(response -> {
+				if (response.getStatus() == Status.OK) {
+					List<JsonNode> coordinates =
+							response.asJson().findValues("coordinates");
+					if (coordinates != null && coordinates.size() > 0) {
+						JsonNode node = coordinates.get(0);
+						Cache.set(key.toString(), node);
+						return resultFromNode(node);
 					}
-					Logger.error(
-							"Geo lookup response status not OK. Key={}, URL={}, Status: {} ({})",
-							key, url, response.getStatus(), response.getStatusText());
-					return null;
-				});
-				return promise.get(1, TimeUnit.MINUTES);
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
+				}
+				Logger.error("Geo lookup failed. Key={}, Params={}, Status: {} ({})",
+						key, requestHolder.getQueryParameters(), response.getStatus(),
+						response.getStatusText());
 				return null;
-			}
+			});
+			String result = promise.get(1, TimeUnit.MINUTES);
+			delay();
+			return result;
 		}
 		return null;
+	}
+
+	private static void delay() {
+		// 6 requests per second. see
+		// https://mapzen.com/documentation/search/api-keys-rate-limits/
+		try {
+			Thread.sleep(1000 / 6);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String resultFromNode(JsonNode node) {
+		String lon = node.get(0).toString();
+		String lat = node.get(1).toString();
+		return this.lookupType == LookupType.LAT ? lat : lon;
 	}
 }
