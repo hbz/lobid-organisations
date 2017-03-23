5d834e17c35422a35cd030e731233cf0b2235f62
Fabian Steeg <fsteeg@gmail.com>
2017-03-23 11:40:16 +0100

    Unify nav bar content and behavior
    See https://github.com/hbz/lobid-resources/issues/281

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -186,6 +187,18 @@ public class Application extends Controller {
 	 */
 	public static String currentLang() {
 		return isEnglish() ? "en" : "de";
+	}
+
+	/**
+	 * @return The current full URI, URL-encoded, or null.
+	 */
+	public static String currentUri() {
+		try {
+			return URLEncoder.encode(request().host() + request().uri(), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			Logger.error("Could not get current URI", e);
+		}
+		return null;
 	}
 
 	/**
