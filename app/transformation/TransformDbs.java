88d290c8f2ddc8cd636d389be4d296189b4a1e4d
Tobias Bülte <tobias.buelte@hbz-nrw.de>
2023-08-07 13:35:36 +0200

    Change workflows and fixes so that no triples are needed

--- app/transformation/TransformDbs.java
+++ app/transformation/TransformDbs.java
@@ -6,9 +6,6 @@ import org.metafacture.metafix.Metafix;
 import org.metafacture.csv.CsvDecoder;
 import org.metafacture.json.JsonEncoder;
 import org.metafacture.io.LineReader;
-import org.metafacture.triples.StreamToTriples;
-import org.metafacture.triples.TripleFilter;
-import org.metafacture.triples.TripleCollect;
 import org.metafacture.io.ObjectWriter;
 import org.metafacture.io.FileOpener;
 import org.metafacture.metafix.Metafix;
@@ -23,22 +20,15 @@ import java.io.FileNotFoundException;
 public class TransformDbs {
 	static void process(final String outputPath, String geoLookupServer) throws FileNotFoundException {
 		final FileOpener opener = new FileOpener();
-		StreamToTriples streamToTriples = new StreamToTriples();
-		streamToTriples.setRedirect(true);
 		opener.setEncoding("UTF-8");
 		final CsvDecoder decoder = new CsvDecoder(',');
 		decoder.setHasHeader(true);
-		final TripleFilter tripleFilter = new TripleFilter();
-		tripleFilter.setSubjectPattern(".+"); // Remove entries without id
 		JsonEncoder encodeJson = new JsonEncoder();
 		encodeJson.setPrettyPrinting(true);
 		opener//
 				.setReceiver(new LineReader())//
 				.setReceiver(decoder)//
 				.setReceiver(new Metafix("conf/fix-dbs.fix"))//
-				.setReceiver(streamToTriples)//
-				.setReceiver(tripleFilter)//
-				.setReceiver(new TripleCollect())//
 				.setReceiver(TransformAll.fixEnriched(geoLookupServer))//
 				.setReceiver(encodeJson)//
 				.setReceiver(TransformAll.esBulk())//
