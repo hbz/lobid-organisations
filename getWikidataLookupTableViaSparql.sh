fe12013d8db74b49110194ee3dd81c56adf43a01
Pascal Christoph <christoph@hbz-nrw.de>
2023-10-05 14:50:17 +0200

    Add test if files size is > 0 before copy file (#482)

--- getWikidataLookupTableViaSparql.sh
+++ getWikidataLookupTableViaSparql.sh
@@ -1,3 +1,8 @@
+# see #482
+
+OUTPUT_FILE="./conf/wikidataLookup.tsv"
+OUTPUT_FILE_TMP=${OUTPUT_FILE}".tmp"
+
 # Items that have an ISIL or DBS ID, add GND ID if existing
 curl --header "Accept: text/tab-separated-values" -G 'https://query.wikidata.org/sparql' --data-urlencode query='
 SELECT ?item ?itemLabel ?isil ?gndId ?dbsId
@@ -9,4 +14,9 @@ WHERE
      OPTIONAL { ?item wdt:P227 ?gndId . } # Add GND ID if in Wikidata.
     SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en". }
 }
-' |sed 's#<##g' | sed 's#^"##g'|sed 's#"\t<#\t#g'|sed 's#>\t"#\t#g' |sed 's#"@..#\t#g'  |sed 's#\t"#\t#g' |sed 's#"\t#\t#g' |sed 's#"\^\^.*##g' |sed 's#\t\t#\t#g' |sed 's#"$##g' > ./conf/wikidataLookup.tsv
+' |sed 's#<##g' | sed 's#^"##g'|sed 's#"\t<#\t#g'|sed 's#>\t"#\t#g' |sed 's#"@..#\t#g'  |sed 's#\t"#\t#g' |sed 's#"\t#\t#g' |sed 's#"\^\^.*##g' |sed 's#\t\t#\t#g' |sed 's#"$##g' > $OUTPUT_FILE_TMP
+
+if [ -s $OUTPUT_FILE_TMP ]; then
+        mv $OUTPUT_FILE_TMP $OUTPUT_FILE
+fi
+
